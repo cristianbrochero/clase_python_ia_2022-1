160c9a0aa8129b8d7a47c27e49918f804259be0a
Cristian Brochero <cbrocher@cuc.edu.co>
2022-03-02 16:54:53 -0500

    variables, operacion, tipos de datos 1

---
+++ CLASE_UNO_PYTHON.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  2 12:49:48 2022
+
+@author: Cristian Brochero
+"""
+
+a = 3
+print(type(a))
+a = "hola"
+print(type(a))
+a = 3.5
+print(type(a))
+a = True
+print(type(a))
+
+print('Hola Mundo')
+print('Hola Mundo dos')
+
+print("I can't do it")
+print('His name is "Cristian"')
+
+# Operaciones
+
+#suma
+a = 5
+b = 2
+c = a + b
+print(c)
+
+#resta
+a = 5
+b = 2
+c = a - b
+print(c)
+
+# multiplicación
+a = 5
+b = 2
+c = a * b
+
+# división
+a = 4
+b = 2
+c = a / b
+
+# división parte entera
+
+a = 5
+b = 2
+c = a//b
+print(c)
+
+# Potencia
+a = 2
+c = a ** 6
+
+# Raiz cuadrada
+a = 16
+b = a ** (1/3)
+
+import math
+raiz = math.sqrt(a)
+print(raiz)
+
+# Tipos de datos
+
+# String str
+
+a = "Hola Mundo"
+a = 'Hola Mundo'
+b = " I can't do it"
+c = 'Alias "Roberto"'
